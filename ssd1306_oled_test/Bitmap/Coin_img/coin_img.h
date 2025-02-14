336f30d47eaa07973c20a6b5bb7eab0ae1167bee
Luong Huu Phuc <Huuphuc1016@gmail.com>
2025-02-15 00:08:23 +0700

    Remove files

--- ssd1306_oled_test/Bitmap/Coin_img/coin_img.h
+++
@@ -1,8 +0,0 @@
-#ifndef COIN_IMG_H
-#define COIN_IMG_H
-
-#include <stdint.h>
-#include <stdio.h>
-
-extern const uint8_t coin_bitmap[];
-#endif //CON_IMG_H
--- ssd1306_oled_test/Bitmap/chili_img/chili_img.h
+++
@@ -1,9 +0,0 @@
-#ifndef CHILI_IMG_H
-#define CHILI_IMG_H
-
-#include <stdint.h>
-#include <stdio.h>
-
-extern const uint8_t chili_bitmap[] ; 
-
-#endif //CHILI_IMG_h
