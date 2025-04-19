9b26ec155e868daaa082101826fbf2fca662b442
Luong Huu Phuc <Huuphuc1016@gmail.com>
2025-04-19 15:19:31 +0700

    vl53l0x_test modify

--- Esp32_origin_project/vl53l0x_test/main/main.c
+++ Esp32_origin_project/vl53l0x_test/main/main.c
@@ -7,6 +7,13 @@
 #include "esp_log.h"
 #include "vl53l0x.h"
 
+//Oled hien thi thong so
+#include "lcd/oled_ssd1306.h"
+#include "i2c/ssd1306_i2c.h"
+#include "ssd1306_generic.h"
+#include "ssd1306_fonts.h"
+#include "ssd1306_1bit.h"
+
 #define SDA_PIN            GPIO_NUM_21
 #define SCL_PIN            GPIO_NUM_22
 #define XSHUT_PIN          GPIO_NUM_5
@@ -14,10 +21,16 @@
 #define I2C_MASTER_CLK     100000
 #define VL53L0X_ADDR       UINT8_C(0x52)
 #define VL53L0X_7BITS_ADDR UINT8_C(0x29)
+#define SSD1306_ADDR       INT8_C(0x3C)
+
+#define CONFIG_PLATFORM_I2C_AVAILABLE
+#define CONFIG_PLATFORM_I2C_ENABLE 
 
 static const char *TAG = "VL53L0X";
-TaskHandle_t vl53l0x_handle = NULL;
+TaskHandle_t vl53l0x_handle = NULL, ssd1306_handle = NULL;
 vl53l0x_t *sensor = NULL;
+static volatile uint16_t distance = 0;
+volatile bool isNewData = false;
 
 esp_err_t scan_i2c_device(uint8_t addr){
   i2c_cmd_handle_t cmd = i2c_cmd_link_create();
@@ -57,9 +70,40 @@ void xshut_reset(){
   vTaskDelay(pdMS_TO_TICKS(10));
 }
 
+void display_task(void *pvParameter){
+  char buf[30];
+  ssd1306_clearScreen();
+  // ssd1306_printFixed(15, 10, "Time of Flight", STYLE_BOLD);
+  // ssd1306_printFixed(45, 10, "VL53L0X", STYLE_NORMAL);
+  
+  uint16_t last_distance = 0xFFFF; //Gia tri ban dau khong hop le
+
+  while(1){
+    if(isNewData && distance != last_distance){
+      last_distance = distance;
+      isNewData = false;
+      ssd1306_clearBlock(0, 40, 128, 48);
+      snprintf(buf, sizeof(buf), "Distance:%umm", distance);
+      ssd1306_printFixed(0, 40, buf, STYLE_NORMAL);
+    }
+    vTaskDelay(pdMS_TO_TICKS(100));
+  }
+}
+
+const char *oled_display_init(void){
+  ssd1306_i2cInitEx(SCL_PIN, SDA_PIN, SSD1306_ADDR);
+  ssd1306_128x64_init();
+  ssd1306_setContrast(UINT8_C(0x7F));
+  ssd1306_setFixedFont(ssd1306xled_font8x16);
+  ssd1306_displayOn();
+
+  return NULL; //Tra ve rong neu khoi tao thanh cong
+}
+
 void sensor_init(void){
   ESP_LOGI(TAG, "Khoi tao cam bien...");
 
+  xshut_reset();
   sensor = vl53l0x_config(I2C_PORT, SCL_PIN, SDA_PIN, XSHUT_PIN, VL53L0X_7BITS_ADDR, true);
   if(!sensor){
     ESP_LOGE(TAG, "Khong tim thay cam bien !");
@@ -77,17 +121,26 @@ void sensor_init(void){
     for(;;) vTaskDelay(1);
   } 
   ESP_LOGI(TAG, "Cam bien khoi tao thanh cong !");
+  vTaskDelay(pdMS_TO_TICKS(100));
 }
 
 void vl53l0x_task(void *pvParameter){
   while(true){
-    uint16_t distance = vl53l0x_readRangeSingleMillimeters(sensor);
+    distance = vl53l0x_readRangeSingleMillimeters(sensor);
     printf("\n%umm", distance);
+    isNewData = true;
     vTaskDelay(pdMS_TO_TICKS(100));
   }
 }
 
 void app_main(){
-  sensor_init();
+  sensor_init(); //Chi khoi tao I2c mot lan cho ca 2 thiet bi 
+  const char *err = oled_display_init();
+  if(err){
+    ESP_LOGE(pcTaskGetName(NULL), "Loi khoi tao man hinh !");
+  }
+  ESP_LOGI(pcTaskGetName(NULL), "Khoi tao man hinh thanh cong !");
+
   xTaskCreatePinnedToCore(vl53l0x_task, "readVL53L0X_task", 1024 * 3, NULL, 1, &vl53l0x_handle, 0);
+  xTaskCreatePinnedToCore(display_task, "display task", 1024 * 3, NULL, 1, &ssd1306_handle, 1);
 }
