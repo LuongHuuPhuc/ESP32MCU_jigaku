e998c68646e0b5ff5a8fd02ff7158197b9d03091
Luong Huu Phuc <Huuphuc1016@gmail.com>
2025-04-19 15:18:53 +0700

    ECG signal test with Esp32-c3 using esp-timer

---
+++ Esp32_c3mini1_project/ad8232_test/main/ad8232_adc.c
@@ -0,0 +1,59 @@
+/**
+ * \name ECG signal measure
+ * \note Chi do raw data, khong can bo loc
+ */
+#include <stdio.h>
+#include <stdint.h>
+#include "esp_err.h"
+#include "esp_log.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "driver/adc.h"
+#include "hal/adc_types.h"
+#include "filter_master_ecg.h"
+#include "esp_timer.h"
+
+#define ADC_CHANNEL ADC_CHANNEL_1 //GPIO1 
+#define ADC_UNIT ADC_UNIT_1
+#define ADC_ATTEN ADC_ATTEN_DB_12 //Tang pham vi do 
+#define ADC_WIDTH ADC_WIDTH_BIT_12
+#define ADC_SAMPLE_RATE 500
+
+static const char *TAG = "ADC";
+TaskHandle_t readADTask_handle = NULL;
+
+void adc_configure(){
+  adc1_config_width(ADC_WIDTH);
+  adc1_config_channel_atten(ADC_CHANNEL, ADC_ATTEN); //Suy hao
+  ESP_LOGI(TAG, "ADC Configured: Channel: %d, Attenuation: %d", ADC_CHANNEL, ADC_ATTEN);
+}
+
+void adc_sampling_callback(void *pvParameter){
+  int sample = adc1_get_raw(ADC_CHANNEL);
+  printf("%d\n", sample);
+}
+
+void esp_timer_setup(void){
+  const esp_timer_create_args_t adc_timer_args = {
+    .callback = &adc_sampling_callback,
+    .arg = NULL,
+    .name = "adc_timer",
+    .dispatch_method = ESP_TIMER_TASK,
+  };
+
+  esp_timer_handle_t adc_timer;
+  ESP_ERROR_CHECK(esp_timer_create(&adc_timer_args, &adc_timer));
+  
+  int64_t interval_us = 1000000 / ADC_SAMPLE_RATE;  //Microseconds
+  ESP_LOGI(TAG, "Bat dau lay mau ADC moi %lld us (~%d Hz)", interval_us, ADC_SAMPLE_RATE);
+  
+  ESP_ERROR_CHECK(esp_timer_start_periodic(adc_timer, interval_us)); //Start timer lap
+}
+
+void app_main(void){
+  adc_configure();
+  esp_timer_setup();
+  while(1){
+    vTaskDelay(pdMS_TO_TICKS(1000)); //Khong lam gi ca, cho thoi gian callback
+  }
+}
